73d046601074c63b6b1ad0eba06a732384b26bbf
adelnizamutdinov <stiggpwnz@gmail.com>
2014-03-03 15:49:37 +0400

    provide rx schedulers

--- app/src/main/java/com/companyname/appname/dagger/UiScopeDaggerModule.java
+++ app/src/main/java/com/companyname/appname/dagger/UiScopeDaggerModule.java
@@ -3,10 +3,17 @@ package com.companyname.appname.dagger;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by adelnizamutdinov on 03/03/2014
@@ -18,4 +25,12 @@ public class UiScopeDaggerModule {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
+
+    @Qualifier @Retention(RetentionPolicy.RUNTIME) public static @interface IOThreadPool {}
+
+    @Provides @IOThreadPool Scheduler provideIOThreadPool() { return Schedulers.io(); }
+
+    @Qualifier @Retention(RetentionPolicy.RUNTIME) public static @interface MainThread {}
+
+    @Provides @MainThread Scheduler provideMainThread() { return AndroidSchedulers.mainThread(); }
 }
