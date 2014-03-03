ee10c4763846bbb17fa11f2995072e243881ba18
adelnizamutdinov <stiggpwnz@gmail.com>
2014-03-03 17:52:13 +0400

    pump up the build tools version

---
+++ app/src/test/java/com/companyname/appname/AppTest.java
@@ -0,0 +1,20 @@
+package com.companyname.appname;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by adelnizamutdinov on 03/03/2014
+ */
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class AppTest {
+    @Test public void testOnCreate() throws Exception {
+        assertTrue(true);
+    }
+}
