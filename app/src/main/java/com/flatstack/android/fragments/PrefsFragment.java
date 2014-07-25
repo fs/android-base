50c027ace875cc6f91424a126dc27fe68d7414af
adelnizamutdinov <stiggpwnz@gmail.com>
2014-07-25 12:24:53 -0500

    return back to mixpanel, fix aapt issue

--- app/src/main/java/com/flatstack/android/fragments/PrefsFragment.java
+++ app/src/main/java/com/flatstack/android/fragments/PrefsFragment.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 import com.flatstack.android.R;
+import com.flatstack.android.dagger.Dagger;
 import com.flatstack.android.utils.ActionBars;
+import com.flatstack.android.utils.MixPanel;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
 
 public class PrefsFragment extends PreferenceFragment {
+    @Inject @NotNull MixPanel mixPanel;
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
@@ -14,9 +22,11 @@ public class PrefsFragment extends PreferenceFragment {
 
     @Override public void onStart() {
         super.onStart();
+        Dagger.inject(this);
         ActionBars.configure(this, actionBar -> {
             ActionBars.homeAsUp(actionBar, true);
             actionBar.setTitle(R.string.settings);
         });
+        mixPanel.track("screen", "name", getClass().getSimpleName());
     }
 }
