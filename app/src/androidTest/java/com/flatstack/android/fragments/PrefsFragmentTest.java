b6811c9a5477ac1a48c3c65b35fc51af8129b74e
adelnizamutdinov <stiggpwnz@gmail.com>
2014-12-12 12:54:37 +0300

    lint warnings on apt

--- app/src/androidTest/java/com/flatstack/android/fragments/PrefsFragmentTest.java
+++ app/src/androidTest/java/com/flatstack/android/fragments/PrefsFragmentTest.java
@@ -1,7 +1,5 @@
 package com.flatstack.android.fragments;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v7.internal.view.menu.ActionMenuItem;
 import com.flatstack.android.R;
 import com.flatstack.android.rx.RxActivity;
 
@@ -17,20 +15,5 @@ public class PrefsFragmentTest extends FragmentTestCase<PrefsFragment> {
     final RxActivity activity = getActivity();
     assertThat(activity.getTitle()).isEqualTo(activity.getString(R.string.settings));
     assertThat(fragment.hasOptionsMenu()).isTrue();
-  }
-
-  public void testBack() {
-    final FragmentManager fm = getActivity().getSupportFragmentManager();
-    fm.beginTransaction()
-        .replace(android.R.id.content, create())
-        .addToBackStack(null)
-        .commit();
-    executePendingTransactions();
-    assertThat(fm.getBackStackEntryCount()).isEqualTo(1);
-
-    fragment.onOptionsItemSelected(
-        new ActionMenuItem(getActivity(), 0, android.R.id.home, 0, 0, ""));
-    executePendingTransactions();
-    assertThat(fm.getBackStackEntryCount()).isEqualTo(0);
   }
 }
