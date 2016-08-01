a33699e8c65ec1f02af250492a60364f6408b6c5
Илья Еремин <partissanei@gmail.com>
2016-08-01 19:31:44 +0300

    remove timber

--- app/src/main/java/com/flatstack/android/utils/TimberCrashReportingTree.java
+++
@@ -1,9 +0,0 @@
-package com.flatstack.android.utils;
-
-import timber.log.Timber;
-
-public class TimberCrashReportingTree extends Timber.HollowTree {
-  @Override public void e(Throwable t, String message, Object... args) {
-    //  Crashlytics.logException(t);
-  }
-}
