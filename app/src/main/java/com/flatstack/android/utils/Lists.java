95817f9487a09d2eff3ac55e04acc5d39d7fb6be
Ilya Eremin <partissanei@gmail.com>
2017-01-08 01:44:49 +0300

    add list helper methods

--- app/src/main/java/com/flatstack/android/utils/Lists.java
+++ app/src/main/java/com/flatstack/android/utils/Lists.java
@@ -1,24 +1,104 @@
 package com.flatstack.android.utils;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.util.Pair;
+
+import org.jetbrains.annotations.Contract;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by adel on 6/7/14
  */
 public class Lists {
-  @SafeVarargs @NonNull public static <T> List<T> mutableOf(@NonNull T... objects) {
-    List<T> list = new ArrayList<>(objects.length);
-    Collections.addAll(list, objects);
-    return list;
-  }
 
-  @SafeVarargs @NonNull public static <T> List<T> add(@NonNull List<T> list,
-                                                      @NonNull T... objects) {
-    Collections.addAll(list, objects);
-    return list;
-  }
+    public static boolean isEmpty(@Nullable List list) {
+        return list == null || list.isEmpty();
+    }
+
+    /**
+     * @return list of T which contains values which filter.call(item) returns true
+     */
+    @Contract(pure = true)
+    @NonNull
+    public static <T> List<T> filterBy(@NonNull List<T> list,
+                                       @NonNull Func1<T, Boolean> filter) {
+        List<T> filteredList = new ArrayList<>();
+        for (T item : list) {
+            if (filter.call(item)) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
+    /**
+     * MUTATE original list
+     */
+    @Contract(pure = false)
+    public static <T> List<T> updateItem(@NonNull List<T> list, @NonNull T item) {
+        int index = list.indexOf(item);
+        if (index >= 0) {
+            list.set(index, item);
+        }
+        return list;
+    }
+
+    @Nullable
+    @Contract(value = "null, null -> null; !null, _ -> !null; _, !null -> !null", pure = true)
+    public static <T> List<T> merge(List<T> list1, List<T> list2) {
+        if (list1 == null && list2 == null) return null;
+        List<T> finalList = new ArrayList<T>();
+        if (list1 != null) {
+            finalList.addAll(list1);
+        }
+        if (list2 != null) {
+            finalList.addAll(list2);
+        }
+        return finalList;
+    }
+
+    @Nullable
+    @Contract(value = "null -> null; !null -> !null", pure = false)
+    public static <T> List<T> uniq(@Nullable List<T> list) {
+        if (list != null) {
+            Set<T> set = new HashSet<>(list);
+            list.clear();
+            list.addAll(set);
+        }
+        return list;
+    }
+
+    @Contract("null, _ -> fail; _, null -> fail")
+    public static <T> void forEach(@NonNull List<T> list, @NonNull Action1<T> action) {
+        for (T item : list) {
+            action.call(item);
+        }
+    }
+
+    /**
+     * @return Pair of two lists. First list contains values which satisfy groupBy
+     * i.e. condition.call(item) returns true. Second list contains rest of items from #list.
+     */
+    @Contract(pure = true, value = "null,_ -> fail; _,null -> fail; null, null -> fail")
+    public static <T> Pair<List<T>, List<T>> groupBy(@NonNull List<T> list,
+                                                     @NonNull Func1<T, Boolean> condition) {
+        List<T> list1 = new ArrayList<>();
+        List<T> list2 = new ArrayList<>();
+        for (T item : list) {
+            if (condition.call(item)) {
+                list1.add(item);
+            } else {
+                list2.add(item);
+            }
+        }
+        return new Pair<>(list1, list2);
+    }
 }
