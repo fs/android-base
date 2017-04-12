c2e90910f33a841e5e144dd7dc94b6e96422a6b8
Ilya Eremin <partissanei@gmail.com>
2017-04-12 22:57:56 +0300

    make toolbar optional

--- app/src/main/java/com/flatstack/android/utils/ui/BaseActivity.java
+++ app/src/main/java/com/flatstack/android/utils/ui/BaseActivity.java
@@ -3,6 +3,7 @@ package com.flatstack.android.utils.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,7 +19,7 @@ import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    @Bind(R.id.toolbar) View toolbar;
+    @Nullable @Bind(R.id.toolbar) View toolbar;
 
     @NonNull public abstract UiInfo getUiInfo();
 
