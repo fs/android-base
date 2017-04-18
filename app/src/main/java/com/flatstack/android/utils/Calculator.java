9ce1a80d8b29e2f13cad1a440e90cb5f61afee00
Ilya Eremin <partissanei@gmail.com>
2017-04-19 00:49:29 +0300

    add calculator

---
+++ app/src/main/java/com/flatstack/android/utils/Calculator.java
@@ -0,0 +1,16 @@
+package com.flatstack.android.utils;
+
+/**
+ * Created by ereminilya on 19/4/17.
+ */
+
+public class Calculator {
+
+    public int plus(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int minus(int number1, int number2) {
+        return number1 - number2;
+    }
+}
